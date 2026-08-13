5196fd1c7b85d2d68ec164a7a6ad9193769b4dcd
Farhan Khalid <farhankhalid17968@gmail.com>
2026-08-13 14:47:31 +0530

    fix: proxy users and webhook routes to port 3001 server

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,14 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
+        source: "/api/users/:path*",
+        destination: "http://200.234.34.240:3001/users/:path*", // Proxy to Auth Server
+      },
+      {
+        source: "/api/webhook/:path*",
+        destination: "http://200.234.34.240:3001/webhook/:path*", // Proxy to Webhook Server
+      },
+      {
         source: "/api/:path*",
         destination: "http://200.234.34.240:5000/:path*", // Proxy to propnex-server (Dashboard API)
       }
