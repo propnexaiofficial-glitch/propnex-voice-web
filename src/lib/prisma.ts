24868ab9144c8e591e762704fb3fc00a48e99d43
Farhan Khalid <farhankhalid17968@gmail.com>
2026-08-21 17:41:18 +0530

    fix(prisma): hardcode datasourceUrl to bypass Vercel env limits

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,6 +7,7 @@ const globalForPrisma = globalThis as unknown as {
 const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL || "mongodb://propnexaiofficial_db_user:KccmbOVh4GsZp1Ok@ac-bwr5uzv-shard-00-00.vuuzm1i.mongodb.net:27017,ac-bwr5uzv-shard-00-01.vuuzm1i.mongodb.net:27017,ac-bwr5uzv-shard-00-02.vuuzm1i.mongodb.net:27017/propnex?ssl=true&authSource=admin&replicaSet=atlas-j95wo5-shard-0&retryWrites=true&w=majority&appName=Cluster0",
     log: process.env.NODE_ENV === "development" ? ["error"] : ["error"],
   });
 
